4a608c047c9151e9806e186ec0e3e3a5bf3fd3fd
Shendul <XiranSonja@Gmail.com>
2014-08-06 09:14:20 -0400

    fixed Stat_type

--- heroclass.py
+++ heroclass.py
@@ -13,10 +13,10 @@ class HERO_CLASS:
   WIZARD = 2 # TODO: implement wizard.
 
 
-  class STAT_TYPE:
+class STAT_TYPE:
   """
-  This class is used to represent an enumlike object. Similar to the above class, this time
-  for attributes.
+  This class is used to represent an enumlike object. Similar to the above
+  class, this time for stats.
   """
   STRENGTH = 1
   AGILITY = 2
@@ -25,34 +25,34 @@ class HERO_CLASS:
 
 
 #Base Stats (level 0 stat, growth per level)
-base_attributes = {
+base_stats = {
   HERO_CLASS.WARRIOR: {
-    'strength': (22, 3),
-    'agility': (14, 1),
-    'wisdom': (9, 1),
-    'constitution': (22, 3)
+    STAT_TYPE.STRENGTH: (22, 3),
+    STAT_TYPE.AGILITY: (14, 1),
+    STAT_TYPE.WISDOM: (9, 1),
+    STAT_TYPE.CONSTITUTION: (22, 3)
   },
   HERO_CLASS.WIZARD: {
-    'strength': (9, 1),
-    'agility': (23, 2),
-    'wisdom': (22, 3),
-    'constitution': (18, 2)
+    STAT_TYPE.STRENGTH: (9, 1),
+    STAT_TYPE.AGILITY: (23, 2),
+    STAT_TYPE.WISDOM: (22, 3),
+    STAT_TYPE.CONSTITUTION: (18, 2)
   }
 }
 
-def getBaseAttributesForHero(hero_class, level):
+def getBaseStatsForHero(hero_class, level):
   """
-  Given a HERO_CLASS and level, return the base attributes for that hero.
+  Given a HERO_CLASS and level, return the base stats for that hero.
   """
   return {
-    'strength': base_attributes[hero_class]['strength'][0] +
-        base_attributes[hero_class]['strength'][1]*level,
-    'agility': base_attributes[hero_class]['agility'][0] +
-        base_attributes[hero_class]['agility'][1]*level,
-    'wisdom': base_attributes[hero_class]['wisdom'][0] +
-        base_attributes[hero_class]['wisdom'][1]*level,
-    'consitution': base_attributes[hero_class]['constitution'][0] +
-        base_attributes[hero_class]['constitution'][1]*level,
+    STAT_TYPE.STRENGTH: base_stats[hero_class][STAT_TYPE.STRENGTH][0] +
+        base_stats[hero_class][STAT_TYPE.STRENGTH][1]*level,
+    STAT_TYPE.AGILITY: base_stats[hero_class][STAT_TYPE.AGILITY][0] +
+        base_stats[hero_class][STAT_TYPE.AGILITY][1]*level,
+    STAT_TYPE.WISDOM: base_stats[hero_class][STAT_TYPE.WISDOM][0] +
+        base_stats[hero_class][STAT_TYPE.WISDOM][1]*level,
+    STAT_TYPE.CONSTITUTION: base_stats[hero_class][STAT_TYPE.CONSTITUTION][0] +
+        base_stats[hero_class][STAT_TYPE.CONSTITUTION][1]*level,
   }
 
 

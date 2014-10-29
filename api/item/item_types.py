dc2ec92da70a153192e1b71a7318b6a6e9b79225
Shendul <XiranSonja@Gmail.com>
2014-10-28 20:28:19 -0400

    added more items

--- api/item/item_types.py
+++ api/item/item_types.py
@@ -1,23 +1,36 @@
 ## item_types.py
 
 class ITEM_TYPE():
-  
+
   ## Melee Weapons
   ONE_HANDED_SWORD = '1hsword'
-  TWO_HANDED_SWORD = '2hsword'
-  POLE_ARM = 'polearm'
   ONE_HANDED_AXE = '1haxe'
   ONE_HANDED_MACE = '1hmace'
+  TWO_HANDED_SWORD = '2hsword'
+  TWO_HANDED_MACE = '2hmace'
+  POLE_ARM = 'polearm'
+  SHIELD = 'shield'
 
   ## Ranged Weapons
+  SLING = 'sling'
+  JAVELIN = 'javelin'
+  CROSSBOW = 'crossbow'
   BOW = 'bow'
 
   ## Magic Weapons
   STAFF = 'staff'
+  WAND = 'wand'
 
   ## Armors
   LIGHT_HELMET = 'lighthelm'
   LIGHT_BODY_ARMOR = 'lightbodyarmor'
+  LIGHT_BOOTS = 'lightboots'
+  LIGHT_GLOVES = 'lightgloves'
+  LIGHT_CLOAK = 'lightcloak'
+  HEAVY_HELMET = 'heavyhelm'
+  HEAVY_BODY_ARMOR = 'heavybodyarmor'
+  HEAVY_BOOTS = 'heavyboots'
+  HEAVY_GLOVES = 'heavygloves'
 
 
   ## Accessories
